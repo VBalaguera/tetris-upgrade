7a3b35e5ceeef9465f0e9c1198520a2ad65dfb4a
Víctor Balaguera <>
2022-01-19 18:52:42 +0100

    commit

---
+++ src/components/Game/Game.js
@@ -0,0 +1,285 @@
+import React from "react";
+import "./Game.css";
+
+export default function Game() {
+  return (
+    <div>
+      <section className="section9">
+        <h1>Now, play</h1>
+        <div className="section9-score-difficulty">
+          <h3>
+            Score:<span id="score">0</span>. Note, this is an unfinished build
+          </h3>
+        </div>
+
+        <div className="game_start">
+          <button id="start-button">Easiest difficulty</button>
+          <button id="normal">Normal difficulty</button>
+          <button id="hard">Hard difficulty</button>
+          <button id="hardest">Hardest difficulty</button>
+          <button id="extreme">Extreme difficulty</button>
+          <button id="restart">
+            Restart<span className="controls_help">: R</span>
+          </button>
+        </div>
+
+        <div className="container">
+          <div className="grid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+            <div className="taken"></div>
+          </div>
+
+          <div className="mini-grid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+
+        <div className="controls">
+          <div className="mobile-controls">
+            <div className="mobile-controls-left-up">
+              <button id="mobile-rotation">
+                Rotate<span className="controls_help">: W</span>
+              </button>
+            </div>
+            <div className="mobile-controls-left-down">
+              <button id="mobile-left">
+                Left<span className="controls_help">: A</span>
+              </button>
+              <button id="mobile-right">
+                Right<span className="controls_help">: D</span>
+              </button>
+            </div>
+            <div className="mobile-controls-up-right">
+              <button id="mobile-down">
+                Down<span className="controls_help">: S</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
